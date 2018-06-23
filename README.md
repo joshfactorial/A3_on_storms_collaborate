29e7b2f897c8baae2dc6ec3ffa9910b87780f708
joshfactorial <joshfactorial@gmail.com>
2018-06-23 14:50:48 -0500

    Testing again

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # A3_on_storms
 An empty startpoint for tracking Assignment 3 collaborations and submissions
 
-Hello World
+Haocong, I went ahead and outlined some basic functions we might need. I don't think this will actually be too
+difficult to do. Maybe next step is to work out the tests that need to be in place? -Josh 6/23
 
--- a3.py
+++ a3.py
@@ -1,3 +1,35 @@
+import pygeodesy as pg
+
+def storm_distance(x):
+    """Computes and ouputs total distance the storm moved
+
+    :param x:
+    :return:
+    """
+    pass
+
+
+def storm_propogation(y):
+    """Calculates the speed, in knots, for each storm sample, based on coordinates and time, then calculates the
+    mean and maximum speed and outputs those quantities
+
+    :param y:
+    :return:
+    """
+    pass
+
+
+def test_hypothesis(z):
+    """Computes the bearing of the storm, then checks the highest level of non-zero radii to see if it is in the
+    quadrant 45-90 degrees from the bearing. If so, it returns true, otherwise returns false
+
+    :param z:
+    :return:
+    """
+    high_wind_is_in_range = False
+    return high_wind_is_in_range
+
+
 def main():
     """
 
